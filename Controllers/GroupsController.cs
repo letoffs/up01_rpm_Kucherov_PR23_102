d744ee3775dbbb50e62b11f473fece4738edc769
letoffs <twinkjjjjkmnb@gmail.com>
2026-02-07 11:31:23 +0700

    GroupsController.cs
    Added GroupsController with endpoints for group management

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,25 @@
+﻿using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IGroupService _groupService;
+
+        public GroupsController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        // GET: api/groups
+        [HttpGet]
+        public async Task<IActionResult> GetAllGroups()
+        {
+            var groups = await _groupService.GetAllGroupsAsync();
+            return Ok(groups);
+        }
+    }
+}
