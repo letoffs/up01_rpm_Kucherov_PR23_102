dd726d4632d4cab4b34da01c9687cb39acfbd381
letoffs <twinkjjjjkmnb@gmail.com>
2026-02-07 11:30:29 +0700

    Add files via upload

---
+++ Services/GroupService.cs
@@ -0,0 +1,34 @@
+﻿using CollegeSchedule.Data;
+using CollegeSchedule.DTO;
+using CollegeSchedule.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeSchedule.Services
+{
+    public class GroupService : IGroupService
+    {
+        private readonly AppDbContext _db;
+
+        public GroupService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<GroupDto>> GetAllGroupsAsync()
+        {
+            var groups = await _db.StudentGroups
+                .Include(g => g.Specialty)
+                .OrderBy(g => g.Course)
+                .ThenBy(g => g.GroupName)
+                .ToListAsync();
+
+            return groups.Select(g => new GroupDto
+            {
+                GroupId = g.GroupId,
+                GroupName = g.GroupName,
+                Course = g.Course,
+                SpecialtyName = g.Specialty.Name
+            }).ToList();
+        }
+    }
+}
